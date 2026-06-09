f1ceb847b092f0639dcc300b90fcd2f707c732aa
Franc-Devel <Agustin.delgado.ojeda@hotmail.com>
2026-06-09 15:27:02 -0300

    Sprint 2 completo y listo para deployar en la rama Main

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import categoryRoutes from "./routes/categories.routes.js";
+import productRoutes from "./routes/products.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+app.use(errorHandler);
+
+app.listen(3000, () => console.log("Server on port 3000"));
